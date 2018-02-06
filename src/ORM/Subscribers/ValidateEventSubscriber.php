728e7d186efa4bdb547c79820f469edb18b4f209
NatePage <nathan.page@loyaltycorp.com.au>
2018-02-07 09:15:00 +1100

    Updated ValidateEventSubscriber not to rely on entity methods to retrieve contents to validate.

--- src/ORM/Subscribers/ValidateEventSubscriber.php
+++ src/ORM/Subscribers/ValidateEventSubscriber.php
@@ -6,14 +6,23 @@ namespace EoneoPay\External\ORM\Subscribers;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Events;
+use EoneoPay\External\Logger\Interfaces\LoggerInterface;
+use EoneoPay\External\Logger\Logger;
 use EoneoPay\External\ORM\Exceptions\DefaultEntityValidationException;
 use EoneoPay\External\ORM\Interfaces\EntityInterface;
 use Illuminate\Contracts\Validation\Factory as ValidationFactory;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Validation\Validator;
+use ReflectionClass;
+use ReflectionException;
 
 class ValidateEventSubscriber implements EventSubscriber
 {
+    /**
+     * @var \EoneoPay\External\Logger\Interfaces\LoggerInterface
+     */
+    private $logger;
+
     /**
      * @var ValidationFactory
      */
@@ -23,10 +32,12 @@ class ValidateEventSubscriber implements EventSubscriber
      * ValidateEventSubscriber constructor.
      *
      * @param \Illuminate\Contracts\Validation\Factory $validationFactory
+     * @param \EoneoPay\External\Logger\Interfaces\LoggerInterface $logger
      */
-    public function __construct(ValidationFactory $validationFactory)
+    public function __construct(ValidationFactory $validationFactory, LoggerInterface $logger)
     {
         $this->validationFactory = $validationFactory;
+        $this->logger = $logger ?? new Logger();
     }
 
     /**
@@ -91,7 +102,7 @@ class ValidateEventSubscriber implements EventSubscriber
 
         try {
             /** @var Validator $validator */
-            $validator = $this->validationFactory->make($entity->toArray(), $entity->getRules());
+            $validator = $this->validationFactory->make($this->getEntityContents($entity), $entity->getRules());
             $validator->validate();
         } catch (ValidationException $exception) {
             $exceptionClass = \method_exists($entity, 'getValidationException')
@@ -105,5 +116,37 @@ class ValidateEventSubscriber implements EventSubscriber
                 $exception->errors()
             );
         }
+    }
+
+    /**
+     * Get entity contents via reflection, this is used so there's no reliance
+     * on entity methods such as toArray().
+     *
+     * @param \EoneoPay\External\ORM\Interfaces\EntityInterface $entity
+     *
+     * @return array
+     */
+    private function getEntityContents(EntityInterface $entity): array
+    {
+        // Get properties available for this model
+        try {
+            $reflection = new ReflectionClass(\get_class($entity));
+        } catch (ReflectionException $exception) {
+            $this->logger->exception($exception);
+
+            // If an exception occurred, return no contents
+            return [];
+        }
+
+        $properties = $reflection->getProperties();
+
+        // Get property values
+        $contents = [];
+        foreach ($properties as $property) {
+            $property->setAccessible(true);
+            $contents[$property->name] = $property->getValue($entity);
+        }
+
+        return $contents;
     }
 }
