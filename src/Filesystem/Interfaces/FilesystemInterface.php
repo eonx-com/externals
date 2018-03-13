911e24f9aee85e446fddd0499d752cf4270f4b60
Scott Dawson <scott@loyaltycorp.com.au>
2018-03-13 15:20:04 +1100

    Add methods to interface

--- src/Filesystem/Interfaces/FilesystemInterface.php
+++ src/Filesystem/Interfaces/FilesystemInterface.php
@@ -5,5 +5,33 @@ namespace EoneoPay\External\Filesystem\Interfaces;
 
 interface FilesystemInterface
 {
-    //
+    /**
+     * Check whether a file exists
+     *
+     * @param string $path
+     *
+     * @return bool
+     */
+    public function exists(string $path): bool;
+
+    /**
+     * Get contents of a file
+     *
+     * @param string $path The path to the file
+     *
+     * @return string
+     *
+     * @throws \EoneoPay\External\Filesystem\Exceptions\FileNotFoundException If file is not found
+     */
+    public function read(string $path): string;
+
+    /**
+     * Write a file to the filesystem
+     *
+     * @param string $path The path to write to
+     * @param string $contents The contents to write to the file
+     *
+     * @return bool
+     */
+    public function write(string $path, string $contents): bool;
 }
