e2962c9c522f55f1161e39d250ab1ae6d57393c0
NatePage <nathan.page@loyaltycorp.com.au>
2018-05-28 14:35:07 +1000

    Make association on associate methods optional.

--- tests/ORM/Stubs/MultiChildEntityStub.php
+++ tests/ORM/Stubs/MultiChildEntityStub.php
@@ -37,9 +37,55 @@ class MultiChildEntityStub extends EntityStub
      * @return \Tests\EoneoPay\Externals\ORM\Stubs\MultiChildEntityStub
      *
      * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidMethodCallException
+     * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidArgumentException
      */
     public function addParent(MultiParentEntityStub $parent): self
     {
         return $this->associateMultiple('parents', $parent, 'children');
+    }
+
+    /**
+     * Add parent with no association.
+     *
+     * @param \Tests\EoneoPay\Externals\ORM\Stubs\MultiParentEntityStub $parent
+     *
+     * @return \Tests\EoneoPay\Externals\ORM\Stubs\MultiChildEntityStub
+     *
+     * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidArgumentException
+     * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidMethodCallException
+     */
+    public function addParentWithNoAssociation(MultiParentEntityStub $parent): self
+    {
+        return $this->associateMultiple('parents', $parent);
+    }
+
+    /**
+     * Add parent with wrong attribute for test purposes.
+     *
+     * @param \Tests\EoneoPay\Externals\ORM\Stubs\MultiParentEntityStub $parent
+     *
+     * @return \Tests\EoneoPay\Externals\ORM\Stubs\MultiChildEntityStub
+     *
+     * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidArgumentException
+     * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidMethodCallException
+     */
+    public function addParentWithWrongAttribute(MultiParentEntityStub $parent): self
+    {
+        return $this->associateMultiple('invalid', $parent, 'children');
+    }
+
+    /**
+     * Add parent with wrong association for test purposes.
+     *
+     * @param \Tests\EoneoPay\Externals\ORM\Stubs\MultiParentEntityStub $parent
+     *
+     * @return \Tests\EoneoPay\Externals\ORM\Stubs\MultiChildEntityStub
+     *
+     * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidArgumentException
+     * @throws \EoneoPay\Externals\ORM\Exceptions\InvalidMethodCallException
+     */
+    public function addParentWithWrongAssociation(MultiParentEntityStub $parent): self
+    {
+        return $this->associateMultiple('parents', $parent, 'invalid');
     }
 }
