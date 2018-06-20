d6c1293fefff0be14deb7cb2d7d73a60209cd740
Scott Dawson <scott@uu.net.nz>
2018-06-20 14:58:44 +1000

    Development (#37)
    * Move serializable interface to entity interface
    
    * Create request and merge with illuminate request
    
    * Add exception to entityfactorymanager to enforce entity creation
    
    * Fix failing test
    
    * Add validatable interface for entity validation
    
    * Add persist method to entity factory manager
    
    * Add level to exception logging
    
    * Make invalid api response extend run time exception

--- src/HttpClient/Exceptions/InvalidApiResponseException.php
+++ src/HttpClient/Exceptions/InvalidApiResponseException.php
@@ -5,10 +5,10 @@ namespace EoneoPay\Externals\HttpClient\Exceptions;
 
 use EoneoPay\Externals\HttpClient\Interfaces\InvalidApiResponseExceptionInterface;
 use EoneoPay\Externals\HttpClient\Interfaces\ResponseInterface;
-use Exception;
+use EoneoPay\Utils\Exceptions\RuntimeException;
 use Throwable;
 
-class InvalidApiResponseException extends Exception implements InvalidApiResponseExceptionInterface
+class InvalidApiResponseException extends RuntimeException implements InvalidApiResponseExceptionInterface
 {
     /**
      * @var \EoneoPay\Externals\HttpClient\Interfaces\ResponseInterface
@@ -28,6 +28,22 @@ class InvalidApiResponseException extends Exception implements InvalidApiRespons
         parent::__construct('', 0, $previous);
 
         $this->response = $response;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getErrorCode(): int
+    {
+        return self::DEFAULT_ERROR_CODE_RUNTIME;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getErrorSubCode(): int
+    {
+        return self::DEFAULT_ERROR_SUB_CODE;
     }
 
     /**
