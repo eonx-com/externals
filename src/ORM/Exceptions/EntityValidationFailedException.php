79bf66590ffa238f696f109e17b1669737a632ed
Scott Dawson <scott@loyaltycorp.com.au>
2019-01-15 09:21:42 +1100

    Use an interface for entity manager validation exceptions

--- src/ORM/Exceptions/EntityValidationFailedException.php
+++ src/ORM/Exceptions/EntityValidationFailedException.php
@@ -3,8 +3,10 @@ declare(strict_types=1);
 
 namespace EoneoPay\Externals\ORM\Exceptions;
 
+use EoneoPay\Externals\ORM\Interfaces\Exceptions\EntityValidationFailedExceptionInterface;
 use EoneoPay\Utils\Exceptions\ValidationException;
 
-abstract class EntityValidationFailedException extends ValidationException
+abstract class EntityValidationFailedException extends ValidationException implements
+    EntityValidationFailedExceptionInterface
 {
 }
---
+++ src/ORM/Interfaces/Exceptions/EntityValidationFailedExceptionInterface.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace EoneoPay\Externals\ORM\Interfaces\Exceptions;
+
+use EoneoPay\Utils\Interfaces\Exceptions\ValidationExceptionInterface;
+
+interface EntityValidationFailedExceptionInterface extends ValidationExceptionInterface
+{
+}
