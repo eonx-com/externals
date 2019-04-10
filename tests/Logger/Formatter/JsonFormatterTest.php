6224c09b3731b7b88616b9f2bf049406aebd86a1
Tim Nagel <tim@nagel.com.au>
2019-04-10 13:18:29 +1000

    Add JsonFormatter that correctly formats dates

---
+++ tests/Logger/Formatter/JsonFormatterTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\EoneoPay\Externals\Logger\Formatter;
+
+use EoneoPay\Externals\Logger\Formatter\JsonFormatter;
+use EoneoPay\Utils\DateTime;
+use Tests\EoneoPay\Externals\TestCase;
+
+/**
+ * @covers \EoneoPay\Externals\Logger\Formatter\JsonFormatter
+ */
+class JsonFormatterTest extends TestCase
+{
+    /**
+     * Tests format date works
+     *
+     * @return void
+     *
+     * @throws \EoneoPay\Utils\Exceptions\InvalidDateTimeStringException
+     */
+    public function testFormatDates(): void
+    {
+        $formatter = new JsonFormatter();
+        $result = $formatter->format([
+            'datetime' => new DateTime('2019-01-01T00:00:00+00:00')
+        ]);
+
+        static::assertSame('{"datetime":"2019-01-01T00:00:00+00:00"}', \trim($result));
+    }
+
+    /**
+     * Tests format date works if the property is not a DateTime
+     *
+     * @return void
+     */
+    public function testFormatDatesString(): void
+    {
+        $formatter = new JsonFormatter();
+        $result = $formatter->format([
+            'datetime' => 'string'
+        ]);
+
+        static::assertSame('{"datetime":"string"}', \trim($result));
+    }
+}
