9d1fb57c92545933aa6064d84fb22c7b18a398ff
Tim Nagel <tim@nagel.com.au>
2019-04-27 19:20:37 +1000

    Accept context array for exception logging

--- src/Logger/Interfaces/LoggerInterface.php
+++ src/Logger/Interfaces/LoggerInterface.php
@@ -3,18 +3,19 @@ declare(strict_types=1);
 
 namespace EoneoPay\Externals\Logger\Interfaces;
 
-use Exception;
 use Psr\Log\LoggerInterface as BaseInterface;
+use Throwable;
 
 interface LoggerInterface extends BaseInterface
 {
     /**
      * Record a caught exception with backtrace
      *
-     * @param \Exception $exception The exception to handle
+     * @param \Throwable $exception The exception to handle
      * @param string|null $level The log level for this exception
+     * @param mixed[]|null $context
      *
      * @return void
      */
-    public function exception(Exception $exception, ?string $level = null): void;
+    public function exception(Throwable $exception, ?string $level = null, ?array $context = null): void;
 }
