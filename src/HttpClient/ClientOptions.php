e260fc3dcddd4985b90991a0c3f6de24876e22f6
Brendon Tyree <brendon@tyree.co.nz>
2019-08-12 13:59:30 +1000

    PYMT-1014: Updated default timeout values, added some additional comments for better understanding

--- src/HttpClient/ClientOptions.php
+++ src/HttpClient/ClientOptions.php
@@ -6,21 +6,38 @@ namespace EoneoPay\Externals\HttpClient;
 use EoneoPay\Externals\HttpClient\Interfaces\ClientOptionsInterface;
 use GuzzleHttp\RequestOptions;
 
+/**
+ * Options passed through to the underlying Guzzle client.
+ *
+ * The timeouts are in the following order in regards to the request lifecycle:
+ *     1) Connection Timeout: The number of seconds the client will wait to connect to the host.
+ *     2) Request Timeout: The number of seconds the client will wait for the first byte from the host.
+ *     3) Read Timeout: The number of seconds the client will wait for the response to stream from the host to client.
+ */
 class ClientOptions implements ClientOptionsInterface
 {
     /**
      * @var float The number of seconds the HTTP client will wait to connect.
+     *
+     * @see http://docs.guzzlephp.org/en/stable/request-options.html#connect-timeout
      */
-    private $connectTimeout = 5;
+    private $connectTimeout = 2;
 
     /**
      * @var float The number of seconds the HTTP client will wait for the response body to
      *          stream before timing out.
+     *
+     * Note: This timeout applies to individual read operations on a streamed body (being, the 'stream' option
+     * passed to Guzzle is set to 'true').
+     *
+     * @see http://docs.guzzlephp.org/en/stable/request-options.html#read-timeout
      */
-    private $readTimeout = 0;
+    private $readTimeout = 3;
 
     /**
      * @var float The number of seconds the HTTP client will wait for a response.
+     *
+     * @see http://docs.guzzlephp.org/en/stable/request-options.html#timeout
      */
     private $requestTimeout = 10;
 
