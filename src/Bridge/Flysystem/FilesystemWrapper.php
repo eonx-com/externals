3548dc5e6100d50e45e79709bbd46d226b9f6b0b
Edward Murrell <edward@loyaltycorp.com.au>
2019-11-25 10:38:10 +1100

    PYMT-1387: Fix whitespace.

--- src/Bridge/Flysystem/FilesystemWrapper.php
+++ src/Bridge/Flysystem/FilesystemWrapper.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace EoneoPay\Externals\Bridge\Flysystem;
 
+use EoneoPay\Externals\Filesystem\Exceptions\FileNotFoundException;
 use EoneoPay\Externals\Filesystem\Interfaces\FilesystemInterface;
+use League\Flysystem\FileNotFoundException as FlysystemFileNotFoundException;
 use League\Flysystem\FilesystemInterface as FlysystemInterface;
 
 class FilesystemWrapper implements FilesystemInterface
@@ -30,9 +32,12 @@ class FilesystemWrapper implements FilesystemInterface
      */
     public function append(string $filename, string $contents): bool
     {
+        /** @var resource $existing */
         $existing = $this->readStream($filename);
 
+        /** @var resource $writeStream */
         $writeStream = \fopen('php://memory', 'ab+');
+
         \stream_copy_to_stream($existing, $writeStream);
         \fwrite($writeStream, $contents);
 
@@ -52,7 +57,7 @@ class FilesystemWrapper implements FilesystemInterface
      */
     public function files(?string $directory = null, ?bool $recursive = null): array
     {
-        return $this->flysystem->listContents($directory, $recursive);
+        return $this->flysystem->listContents($directory ?? '', $recursive ?? false);
     }
 
     /**
@@ -65,10 +70,16 @@ class FilesystemWrapper implements FilesystemInterface
 
     /**
      * {@inheritdoc}
+     *
+     * @throws \EoneoPay\Externals\Filesystem\Exceptions\FileNotFoundException
      */
     public function read(string $filename): string
     {
-        return $this->flysystem->read($filename);
+        try {
+            return $this->flysystem->read($filename);
+        } catch (FlysystemFileNotFoundException $exception) {
+            throw new FileNotFoundException($exception->getMessage(), $exception->getCode(), $exception);
+        }
     }
 
     /**
