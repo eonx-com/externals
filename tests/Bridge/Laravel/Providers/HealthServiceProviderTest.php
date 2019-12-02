6436deef62ddad59b557c49743c19ef8a1a1a57f
Brendon Tyree <brendon@tyree.co.nz>
2019-12-03 10:02:13 +1100

    PYMT-1479: Updated service provider test, again

--- tests/Bridge/Laravel/Providers/HealthServiceProviderTest.php
+++ tests/Bridge/Laravel/Providers/HealthServiceProviderTest.php
@@ -6,7 +6,6 @@ namespace Tests\EoneoPay\Externals\Bridge\Laravel\Providers;
 use EoneoPay\Externals\Bridge\Laravel\Providers\HealthServiceProvider;
 use EoneoPay\Externals\Health\Health;
 use EoneoPay\Externals\Health\Interfaces\HealthInterface;
-use ReflectionClass as ReflectionClass;
 use Tests\EoneoPay\Externals\Stubs\Health\HealthCheckStub;
 use Tests\EoneoPay\Externals\Stubs\Vendor\Illuminate\Contracts\Foundation\ApplicationStub;
 use Tests\EoneoPay\Externals\TestCase;
@@ -20,16 +19,13 @@ class HealthServiceProviderTest extends TestCase
      * Test that service provider registers the health service into the Laravel application.
      *
      * @return void
-     *
-     * @throws \ReflectionException
      */
     public function testRegister(): void
     {
         $application = new ApplicationStub();
-        $application->tag([HealthCheckStub::class], ['externals_healthcheck']);
-        $healthReflection = new ReflectionClass(Health::class);
-        $checksProperty = $healthReflection->getProperty('checks');
-        $checksProperty->setAccessible(true);
+        $healthCheck = new HealthCheckStub('Test Check', 'test-check');
+        $application->instance(HealthCheckStub::class, $healthCheck);
+        $application->tag(HealthCheckStub::class, ['externals_healthcheck']);
 
         // Register services
         (new HealthServiceProvider($application))->register();
@@ -38,7 +34,8 @@ class HealthServiceProviderTest extends TestCase
         self::assertInstanceOf(Health::class, $application->get(HealthInterface::class));
         /** @var \EoneoPay\Externals\Health\Interfaces\HealthInterface $health */
         $health = $application->get(HealthInterface::class);
-        self::assertCount(1, $checksProperty->getValue($health));
-        self::assertInstanceOf(HealthCheckStub::class, $checksProperty->getValue($health)[0]);
+        $result = $health->extended();
+        self::assertCount(1, $result->getServices());
+        self::assertArrayHasKey('test-check', $result->getServices());
     }
 }
