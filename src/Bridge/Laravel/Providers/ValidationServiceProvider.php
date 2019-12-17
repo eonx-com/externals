0bf29b8920f2502658a017541df32b803e0114b1
Tim Nagel <tim@nagel.com.au>
2019-12-17 15:05:14 +1100

    Fix service binding

--- src/Bridge/Laravel/Providers/ValidationServiceProvider.php
+++ src/Bridge/Laravel/Providers/ValidationServiceProvider.php
@@ -6,6 +6,7 @@ namespace EoneoPay\Externals\Bridge\Laravel\Providers;
 use EoneoPay\Externals\Bridge\Laravel\IlluminateValidator;
 use EoneoPay\Externals\Bridge\Laravel\Validator;
 use EoneoPay\Externals\Validator\Interfaces\ValidatorInterface;
+use Illuminate\Contracts\Container\Container;
 use Illuminate\Contracts\Validation\Factory as FactoryInterface;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Factory;
@@ -27,13 +28,13 @@ final class ValidationServiceProvider extends ServiceProvider
         // Overload validator factory to add our own rules in
         $this->app->extend(
             FactoryInterface::class,
-            static function (FactoryInterface $factory): FactoryInterface {
+            static function (FactoryInterface $factory, Container $app): FactoryInterface {
                 if ($factory instanceof Factory === true) {
-                    $factory->resolver(static function ($translator, $data, $rules, $messages, $customAttributes) {
+                    $factory->resolver(static function ($translator, $data, $rules, $messages, $customAttributes) use ($app) {
                         // @codeCoverageIgnoreStart
                         // Hack to return our validator
                         return new IlluminateValidator(
-                            $this->app->make('validator_cache'),
+                            $app->make('validator_cache'),
                             $translator,
                             $data,
                             $rules,
