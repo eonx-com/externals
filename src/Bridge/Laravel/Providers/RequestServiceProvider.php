1c9ec625a8bc0215545b91e035adf942296a1591
edwardmurrell-loyaltycorp <44623821+edwardmurrell-loyaltycorp@users.noreply.github.com>
2019-12-18 16:07:30 +1100

    PYMT-1705: Undo Requests as Singleton. (#105)

--- src/Bridge/Laravel/Providers/RequestServiceProvider.php
+++ src/Bridge/Laravel/Providers/RequestServiceProvider.php
@@ -21,7 +21,7 @@ final class RequestServiceProvider extends ServiceProvider
     public function register(): void
     {
         // Interface for incoming http requests
-        $this->app->singleton(RequestInterface::class, function (): Request {
+        $this->app->bind(RequestInterface::class, function (): Request {
             // Create env instance
             $env = $this->app->make(Env::class);
 
